c0e9d1e70fcd949bad0586e408e7a26cf8e8c606
ratuloffical837-beep <ratuloffical837@gmail.com>
2025-12-04 23:40:47 +0600

    Update config.py
    BOT_TOKEN = "8556814936:AAHKmcyw5RwqDdP_oT_7dH7raAU3-H-LyNY"
    
    # Render থেকে পাওয়া URL বসাও (https://something.onrender.com)
    WEBHOOK_BASE = "https://telegram-webhook-bot-r3rp.onrender.com"

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "PUT-YOUR-BOT-TOKEN-HERE"
+BOT_TOKEN = "8556814936:AAHKmcyw5RwqDdP_oT_7dH7raAU3-H-LyNY"
 
 # Render থেকে পাওয়া URL বসাও (https://something.onrender.com)
-WEBHOOK_BASE = "PUT-YOUR-RENDER-URL-HERE"
+WEBHOOK_BASE = "https://telegram-webhook-bot-r3rp.onrender.com"
